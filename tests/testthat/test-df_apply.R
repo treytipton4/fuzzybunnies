0a74dac5c0ec829e2296367899a23b379dff4a6d
Brent Gin <bg32@calvin.edu>
2023-03-17 17:12:59 -0400

    Program edits commit

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -1,4 +1,15 @@
-x <- "alfa,bravo,charlie,delta"
+Name <- c("Jon", "Bill", "Maria", "Ben", "Tina")
+Age <- c(23, 41, 32, 58, 26)
+df <- data.frame(Name, Age)
+
+Name <- c("Jon", "Bill", "Maria", "Ben", "Tina")
+Age <- c(28, 46, 37, 63, 31)
+df2 <- data.frame(Name, Age)
+
+increment <- function(x) {
+  x <- x + 5
+}
+
 test_that("dataset is modified", {
-  expect_equal(df_apply(x, strsplit1()), x)
+  expect_equal(df_apply(df, increment, is.numeric), df2)
 })
