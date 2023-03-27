d7af4759d651863a8b742e06a23f1a7ebc8a5814
Trey Tipton <tct5@calvin.edu>
2023-03-27 00:58:15 -0400

    documented the datasets using roxygen and made some small changes to examples in function documentation

---
+++ R/data.R
@@ -0,0 +1,57 @@
+#' First data frame in the fuzzybunnies package
+#'
+#' Basic data used to test the function df_apply()
+#'
+#' @format ## `xyz_fuzzybunnies`
+#' A data frame with 3 rows and 3 columns:
+#' \describe{
+#'   \item{x}{values}
+#'   \item{y}{more values}
+#'   \item{z}{strings}
+#' }
+#' @source Trey created in R
+"xyz_fuzzybunnies"
+
+
+#' Second data frame in the fuzzybunnies package
+#'
+#' Basic data used to test the function df_apply()
+#'
+#' @format ## `df_fuzzybunnies`
+#' A data frame with 5 rows and 2 columns:
+#' \describe{
+#'   \item{Name}{list of names}
+#'   \item{Age}{list of their ages}
+#' }
+#' @source Brent created in R
+"df_fuzzybunnies"
+
+
+#' Marketing data from kaggle
+#'
+#' A public github dataset that contains marketing data about some unknown food company
+#'
+#' @format ## `marketing_data`
+#' A data frame with 2,205 rows and 39 columns:
+#' \describe{
+#'   \item{Income}{list of particpant incomes}
+#'   \item{Kidhome}{1 if Kid in home, 0 if not}
+#'   \item{Teen}{1 if Teen in home, 0 if not}
+#'   ...
+#' }
+#' @source <https://raw.githubusercontent.com/nailson/ifood-data-business-analyst-test/master/ifood_df.csv>
+"marketing_data"
+
+
+#' Annual mean temperatures in Ann Arbor, Michigan
+#'
+#' Comes from the U.S. Historical Climatology Network
+#'
+#' @format ## `aatemp`
+#' A data frame with 115 rows and 2 columns:
+#' \describe{
+#'   \item{year}{Year}
+#'   \item{temp}{Temperature in degrees F in Ann Arbor}
+#' }
+#' @source From the package "faraway"
+"aatemp"
